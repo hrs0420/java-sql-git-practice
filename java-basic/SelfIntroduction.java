6c7537e46f2529327f1d4ca0abf8683615d2fa23
hrs0420 <takahiroshi0420@gmail.com>
2026-07-07 11:18:56 +0900

    自己紹介プログラムに変数を追加

--- java-basic/SelfIntroduction.java
+++ java-basic/SelfIntroduction.java
@@ -1,11 +1,18 @@
 public class SelfIntroduction{
 	public static void main(String[] args) {
 		String name = "高宮浩志";
-		String goal = "Java、SQL、Gitを学んで入社前研修に備える";
+		int age = 26;
 		String currentStudy = "職業訓練校でPythonを学習中";
+		String goal = "11月の入社までにJava、SQL、Gitを自走できる状態にする";
+		String firstApp = "営業実績管理アプリ";
+		boolean isLearningJava = true;
 		
+		System.out.println("====自己紹介====");
 		System.out.println("名前：" + name);
+		System.out.println("年齢：" + age + "歳");
 		System.out.println("現在：" + currentStudy);
 		System.out.println("目標：" + goal);
+		System.out.println("作りたいアプリ：" + firstApp);
+		System.out.println("Java学習中：" + isLearningJava);
 	}
 }
