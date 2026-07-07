b881eabd3a70d849b99f06f5c6352dd9eda3ff9d
hrs0420 <takahiroshi0420@gmail.com>
2026-07-07 10:53:35 +0900

    Javaの自己紹介プログラムを追加

---
+++ java-basic/SelfIntroduction.java
@@ -0,0 +1,11 @@
+public class SelfIntroduction{
+	public static void main(String[] args) {
+		String name = "高宮浩志";
+		String goal = "Java、SQL、Gitを学んで入社前研修に備える";
+		String currentStudy = "職業訓練校でPythonを学習中";
+		
+		System.out.println("名前：" + name);
+		System.out.println("現在：" + currentStudy);
+		System.out.println("目標：" + goal);
+	}
+}
