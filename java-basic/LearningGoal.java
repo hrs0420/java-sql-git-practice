b50cc636ab22a820bb8f5993fffabc314ea7c359
hrs0420 <takahiroshi0420@gmail.com>
2026-07-07 11:43:48 +0900

    7月7日の学習内容の追加

---
+++ java-basic/LearningGoal.java
@@ -0,0 +1,14 @@
+public class LearningGoal {
+	public static void main(String[] args) {
+		String language = "Java";
+		String database = "SQL";
+		String tool = "Git";
+		int studyMinutes = 60;
+
+		System.out.println("====今日の学習内容====");
+		System.out.println("言語：" + language);
+		System.out.println("データベース：" + database);
+		System.out.println("ツール：" + tool);
+		System.out.println("学習時間：" + studyMinutes);
+	}
+}
