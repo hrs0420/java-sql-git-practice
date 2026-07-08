8333efff4595ca9168fb5f012d02ca2122abafc3
hrs0420 <takahiroshi0420@gmail.com>
2026-07-08 15:58:45 +0900

    for文の基本練習を追加

---
+++ java-basic/LoopPractice.java
@@ -0,0 +1,7 @@
+public class LoopPractice {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++){
+            System.out.println(i + "回目のJava学習");
+        }
+    }
+}
