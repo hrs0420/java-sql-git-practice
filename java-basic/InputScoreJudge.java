e9e33d1f01a0a843eed79dac3886012779c49775
hrs0420 <takahiroshi0420@gmail.com>
2026-07-09 11:48:38 +0900

    Scannerで点数入力判定を追加

---
+++ java-basic/InputScoreJudge.java
@@ -0,0 +1,24 @@
+//スキャナーのインポート
+import java.util.Scanner; 
+public class InputScoreJudge {
+    public static void main(String[] args) {
+        //Scannerの準備
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("点数を入力してください：");
+        
+        //入力された整数をscoreに入れる
+        int score = scanner.nextInt();
+        System.out.println("入力された点数：" + score);
+
+        if (score >= 80){
+            System.out.println("評価：A");
+        } else if (score >= 60){
+            System.out.println("評価：B");
+        } else {
+            System.out.println("評価：C");
+        }
+
+        scanner.close();
+    }
+}
